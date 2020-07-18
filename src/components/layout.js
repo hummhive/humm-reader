09f21ae4871dce4b261d7237d6b827c3f6148301
Pedro <pedro@fusionbreak.com>
2020-07-18 13:14:35 -0400

    feat: Updates layout component and uses bootstrap

--- src/components/layout.js
+++ src/components/layout.js
@@ -7,39 +7,19 @@
 
 import React from "react"
 import PropTypes from "prop-types"
-import { useStaticQuery, graphql } from "gatsby"
-
 import Header from "./header"
+import { FaPlusCircle } from 'react-icons/fa';
+import "./bootstrap.min.css";
 import "./layout.css"
-
 const Layout = ({ children }) => {
-  const data = useStaticQuery(graphql`
-    query SiteTitleQuery {
-      site {
-        siteMetadata {
-          title
-        }
-      }
-    }
-  `)
-
   return (
     <>
-      <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0 1.0875rem 1.45rem`,
-        }}
-      >
+      <Header />
         <main>{children}</main>
-        <footer>
-          © {new Date().getFullYear()}, Built with
-          {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+        <footer className="container">
+          The Rolf's Newsletter is Powered by the Humm Reader <br />
+        <a className="start-creating" href="https://humm.earth"><FaPlusCircle /> Start Creating</a>
         </footer>
-      </div>
     </>
   )
 }
