aa30594e1df31eaf457ba839d4bfbab5fb9986b4
davidRichard23 <d2391@me.com>
2020-12-16 21:04:56 -0900

    chore: disable nested public folders

--- src/gatsby/gatsby-node.js
+++ src/gatsby/gatsby-node.js
@@ -3,43 +3,43 @@
  *
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
-const path = require("path")
-const fs = require("fs-extra")
-const hiveConfig = require("./content/hive-config.json")
+// const path = require("path")
+// const fs = require("fs-extra")
+// const hiveConfig = require("./content/hive-config.json")
 const indexJSON = require("./content/index.json")
 
 // preserve other hive's build files while also deleting the hive build files
 // corresponding to the currently running build
-exports.onPreInit = () => {
-  if (process.argv[2] === "build") {
-    const buildPath = path.join(__dirname, "public", hiveConfig.id)
-    if (fs.existsSync(buildPath)) fs.removeSync(buildPath, { recursive: true })
-    if (fs.existsSync(path.join(__dirname, "public-tmp")))
-      fs.removeSync(path.join(__dirname, "public-tmp"), { recursive: true })
+// exports.onPreInit = () => {
+//   if (process.argv[2] === "build") {
+//     const buildPath = path.join(__dirname, "public", hiveConfig.id)
+//     if (fs.existsSync(buildPath)) fs.removeSync(buildPath, { recursive: true })
+//     if (fs.existsSync(path.join(__dirname, "public-tmp")))
+//       fs.removeSync(path.join(__dirname, "public-tmp"), { recursive: true })
 
-    fs.mkdirSync(path.join(__dirname, "public-tmp"))
+//     fs.mkdirSync(path.join(__dirname, "public-tmp"))
 
-    if (fs.existsSync(path.join(__dirname, "public")))
-      fs.renameSync(
-        path.join(__dirname, "public"),
-        path.join(__dirname, "public-tmp")
-      )
-  }
-}
+//     if (fs.existsSync(path.join(__dirname, "public")))
+//       fs.renameSync(
+//         path.join(__dirname, "public"),
+//         path.join(__dirname, "public-tmp")
+//       )
+//   }
+// }
 
-// move the new build files to the temp public folder and then move everything
-// back to the public folder
-exports.onPostBuild = function () {
-  fs.renameSync(
-    path.join(__dirname, "public"),
-    path.join(__dirname, "public-tmp", hiveConfig.id)
-  )
-  fs.mkdirSync(path.join(__dirname, "public"))
-  fs.renameSync(
-    path.join(__dirname, "public-tmp"),
-    path.join(__dirname, "public")
-  )
-}
+// // move the new build files to the temp public folder and then move everything
+// // back to the public folder
+// exports.onPostBuild = function () {
+//   fs.renameSync(
+//     path.join(__dirname, "public"),
+//     path.join(__dirname, "public-tmp", hiveConfig.id)
+//   )
+//   fs.mkdirSync(path.join(__dirname, "public"))
+//   fs.renameSync(
+//     path.join(__dirname, "public-tmp"),
+//     path.join(__dirname, "public")
+//   )
+// }
 
 exports.onCreateBabelConfig = p => {
   if (process.env.NODE_ENV !== "development") {
